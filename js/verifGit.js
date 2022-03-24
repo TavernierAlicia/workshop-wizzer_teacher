4578e3766afd58a94043eccd1b75e31479106743
«Alicia» <alicia@ringover.com>
2022-03-24 22:52:03 +0100

    add style

--- js/verifGit.js
+++ js/verifGit.js
@@ -8,11 +8,14 @@ window.addEventListener("load", _ => {
         link = repo.textContent.replace(regexGitHub, 'api.github.com/repos')
         fetch(link).then(res => {
             if (res.ok) {
+                console.log("green")
                 repo.style.color='#40A133'
             } else {
+                console.log("red")
                 repo.style.color='#A53636'
             }
         }).catch(_ => {
+            console.log("red")
             repo.style.color='#A53636'
         })
     }
